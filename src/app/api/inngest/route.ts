fa0c314571d1c65ee1e755f3b568499377e381c6
zxzAli <blackbearddevss@gmail.com>
2025-10-15 14:40:55 -0400

    07 ai provider

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,12 +1,12 @@
 import { inngest } from "@/inngest/client";
 import { serve } from "inngest/next";
-import { helloWorld } from "./functions";
+import { executeAI } from "../../../inngest/functions";
 
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    helloWorld
+    executeAI
   ],
 });
---
+++ src/inngest/functions.ts
@@ -0,0 +1,24 @@
+import { inngest } from "@/inngest/client";
+
+
+import { openai } from '@ai-sdk/openai';
+import { generateText } from 'ai';
+export const executeAI = inngest.createFunction(
+  { id: "execute-ai" },
+  { event: "execute/ai" },
+  async ({ event, step }) => {
+
+    const {steps} = await step.ai.wrap( 'Generating Text', generateText,{
+      model: openai('gpt-4.1'),
+      system:'You are a helpful assistant that helps people find information.',
+      prompt: 'Write a short describtion about  cbr honda bikes.',
+
+
+    });
+
+    return steps;
+
+
+  
+  },
+);
